b4f49bb594ef6b8dcadf277922a9c880c73a6e9b
Thomas Frankliyn <thomasfrankliyn@gmail.com>
2021-03-04 08:06:59 -0300

    primeiro Commit

---
+++ src/components/CompletedChallenges.tsx
@@ -0,0 +1,15 @@
+import { useContext } from 'react';
+import { challengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/CompletedChallenges.module.css';
+
+export function CompletedChallenges(){
+
+    const { challengesCompleted } = useContext(challengesContext);
+
+    return(
+        <div className = {styles.completedChallengesContainer}>
+            <span>Desafios Completos</span>
+            <span>{challengesCompleted}</span>
+        </div>
+    );
+}
